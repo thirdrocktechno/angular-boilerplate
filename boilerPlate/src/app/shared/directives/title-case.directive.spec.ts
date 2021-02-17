fec8556e00b538974509e54088a189fec539ff57
vsbhatt <vidhib@thirdrocktechkno.com>
2021-02-17 13:10:00 +0530

    testing files removed

--- boilerPlate/src/app/shared/directives/title-case.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TitleCaseDirective } from './title-case.directive';
-
-describe('TitleCaseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TitleCaseDirective();
-    expect(directive).toBeTruthy();
-  });
-});
