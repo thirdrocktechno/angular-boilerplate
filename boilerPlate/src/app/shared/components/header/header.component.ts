ed40e3d666e11e5ff19bc58abfee78b752df5164
vsbhatt <vidhib@thirdrocktechkno.com>
2021-02-17 10:28:36 +0530

    ngclass and extra code removed

--- boilerPlate/src/app/shared/components/header/header.component.ts
+++ boilerPlate/src/app/shared/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  showProfile = false;
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
