401fad4036e8562b6ef8b009e57a9d8006d63790
vsbhatt <vidhib@thirdrocktechkno.com>
2021-02-18 14:20:36 +0530

    toastr error for animation module removed

--- boilerPlate/src/app/app.module.ts
+++ boilerPlate/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     ToastrModule.forRoot(),
     NgxSpinnerModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
